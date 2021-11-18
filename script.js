a63ac25ea26603533fc16886544e0c546ade8a8d
Danylo Desiatnyk <danylode@tahometer.com>
2021-11-18 17:52:46 +0200

    Task 2 done

--- script.js
+++ script.js
@@ -37,13 +37,30 @@ function renderTask(task){
     newTaskCheckBoxElement.classList.add('task-done');
     newTaskCheckBoxElement.type = 'checkbox';
 
+    //Add done event on checkbox
+    newTaskCheckBoxElement.addEventListener('change', () => {
+        newTaskNameElement.classList.toggle('task-name-done', newTaskCheckBoxElement.checked);
+        newTaskDateElement.classList.toggle('date-red', newTaskCheckBoxElement.checked);
+    })
+
     if(task.taskDone == true){
         newTaskNameElement.classList.add('task-name-done');
         newTaskCheckBoxElement.checked = true;
     }
 
+    //Add delete button
+    let newTaskDeleteButton = document.createElement('button');
+    newTaskDeleteButton.classList.add('task-delete-button');
+    newTaskDeleteButton.innerText = 'X';
+    newTaskDeleteButton.addEventListener('click', () => {
+        newTaskElement.remove();
+        tasks.splice(tasks.indexOf(tasks),1);
+        console.log(tasks);
+    })
+
     newTaskStatusElement.appendChild(newTaskNameElement);
     newTaskStatusElement.appendChild(newTaskCheckBoxElement);
+    newTaskStatusElement.appendChild(newTaskDeleteButton);
 
     //Task description add
     let newTaskDescriptionElement = document.createElement('h2');
@@ -58,12 +75,17 @@ function renderTask(task){
     }
     newTaskDateElement.innerText = task.taskDate.toISOString().split('T')[0];
 
+    //add change event on showInProgress button
+    let showAll = document.querySelector('#show-all-checkbox');
+    showAll.addEventListener('change', () => {
+        newTaskElement.classList.toggle('nonvisible', (newTaskCheckBoxElement.checked && !showAll.checked));
+    })
 
-    newTaskElement.appendChild(newTaskStatusElement);
     newTaskElement.appendChild(newTaskStatusElement);
     newTaskElement.appendChild(newTaskDescriptionElement);
     newTaskElement.appendChild(newTaskDateElement);
     tasksElement.appendChild(newTaskElement);
 }
 
-tasks.forEach(renderTask)
+
+tasks.forEach(renderTask);
